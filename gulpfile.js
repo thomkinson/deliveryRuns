1505781b3b90476c19dc0ffec74a46bf1f5fa33e
Matthew Thompson <mthompson@bcc.com.au>
2017-01-26 21:37:22 +1100

    Built gulp tasks to build js and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,69 @@
-// Load Node Modules/Plugins
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import browserSync from 'browser-sync';
-import del from 'del';
-var gutil = require('gulp-util');
+(() => {
+    'use strict';
 
-const reload = browserSync.reload;
-const $ = gulpLoadPlugins();
+    const args = require('yargs').argv,
+          gulp = require('gulp'),
+          $ = require('gulp-load-plugins')({ rename: { 'gulp-if': 'gulpif' } }),
+          es = require('event-stream'),
+          del = require('del'),
+          gutil = require('gulp-util'),
+          browserSync = require('browser-sync'),
+          fs = require('fs');
 
-// Remove existing docs and dist build
-gulp.task('clean', del.bind(null, ['docs/dist', 'dist']));
+    var cleanCSS = require('gulp-clean-css');
 
-// Build LibSass files
-gulp.task('styles', function() {
-    gulp.src('./scss/bootstrap.scss')
-        .pipe($.plumber())
-        .pipe($.sourcemaps.init())
-        .pipe($.sass().on('error', $.sass.logError))
-        .pipe($.sourcemaps.write('.'))
-        .pipe($.autoprefixer({browsers: ['last 1 version']}))
-        .pipe($.rename({ suffix: '.min' }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe($.cleanCSS({ compatibility: '*' }))
-        .pipe(gulp.dest('dist/css'));
-});
+    const reload = browserSync.reload;
 
-// Build JavaScript files 
-gulp.task('scripts', function() {
-    return gulp.src(['js/src/util.js', 'js/src/alert.js', 'js/src/button.js', 'js/src/carousel.js', 'js/src/collapse.js', 'js/src/dropdown.js', 'js/src/modal.js', 'js/src/scrollspy.js', 'js/src/tab.js', 'js/src/tooltip.js', 'js/src/popover.js'])
-        .pipe($.babel())
-        .pipe($.concat('./bootstrap.js'))
-        .pipe(gulp.dest('dist/js'));
-});
+    // Remove existing docs and dist build
+    gulp.task('clean', del.bind(null, ['docs/dist', 'dist']));
 
-// Watch tasks
+    // Build LibSass files
+    gulp.task('styles', function() {
+        gulp.src('./bower_components/bootstrap/scss/bootstrap.scss')
+            .pipe($.plumber())
+            .pipe($.sourcemaps.init())
+            .pipe($.sass().on('error', $.sass.logError))
+            .pipe($.autoprefixer({browsers: ['last 1 version']}))
+            .pipe($.rename({ suffix: '.min' }))
+            .pipe(gulp.dest('css'))
+            .pipe(cleanCSS({ compatibility: '*' }))
+            .pipe($.sourcemaps.write('.'))            
+            .pipe(gulp.dest('css'));
+    });
 
-gulp.task('watch', function() {     
-    gulp.watch('scss/*.scss', ['styles']);
-    gulp.watch('js/src/*.js', ['scripts']);
-});
+    // Build JavaScript files 
+    gulp.task('scripts', function() {
+        return gulp.src(['./bower_components/bootstrap/js/src/util.js', 
+                         './bower_components/bootstrap/js/src/alert.js', 
+                         './bower_components/bootstrap/js/src/button.js', 
+                         './bower_components/bootstrap/js/src/carousel.js', 
+                         './bower_components/bootstrap/js/src/collapse.js', 
+                         './bower_components/bootstrap/js/src/dropdown.js', 
+                         './bower_components/bootstrap/js/src/modal.js', 
+                         './bower_components/bootstrap/js/src/scrollspy.js', 
+                         './bower_components/bootstrap/js/src/tab.js', 
+                         './bower_components/bootstrap/js/src/tooltip.js', 
+                         './bower_components/bootstrap/js/src/popover.js',
+                         './bower_components/bootstrap/dist/js/bootstrap.js'])
+            .pipe($.babel({ presets: ['es2015']}))
+            .pipe($.sourcemaps.init())
+            .pipe($.plumber())
+            .pipe($.uglify({ preserveComments: 'license' }))
+            .pipe($.rename({ suffix: '.min' }))
+            .pipe($.sourcemaps.write('maps'))
+            .pipe(gulp.dest('js'));
+    });
 
-gulp.task('dist', ['styles', 'scripts']);
+    // Watch tasks
 
-gulp.task('default', ['clean'], () => {
-    gulp.start('dist');
-});
+    gulp.task('watch', function() {     
+        gulp.watch('scss/*.scss', ['styles']);
+        gulp.watch('js/src/*.js', ['scripts']);
+    });
+
+    gulp.task('dist', ['styles', 'scripts']);
+
+    gulp.task('default', ['clean'], () => {
+        gulp.start('dist');
+    });
+})();
